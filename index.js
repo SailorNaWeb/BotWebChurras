850470e72ac4fccfc4c42c76b59d8fa692baa070
sailornaweb <rafa.juck@outlook.com>
2026-01-09 10:26:50 -0300

    Estrutura básica do bot (copiada fds) e negócio para mensagem anônima

--- index.js
+++ index.js
@@ -1,10 +1,110 @@
-const {Client, Events, GatewayIntentBits} = require('discord.js');
-const {token} = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, Partials, GatewayIntentBits, MessageFlags, PermissionsBitField, ActivityType } = require('discord.js');
+const { token } = require('./config.json');
 
-const cliente = new Client({ intents: [GatewayIntentBits.Guilds]});
+let anonimos = new Map();
 
-cliente.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+const client = new Client({ 
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+	partials: [Partials.Channel, Partials.Message]
 });
 
-cliente.login(token);
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	client.user.setPresence({ activities: [{
+		name: '🚾 Recebendo mensagens anonimatas',
+		type: ActivityType.Playing
+	}] });
+});
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
+});
+
+client.on(Events.MessageCreate, async (message) => {
+	if (message.author.bot) return;
+
+	if (message.guild) {
+		if (message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+			if (message.content.startsWith('wc.')) {
+				await message.reply('Comandos wc');
+				return;
+			}
+		}
+		console.log(`${message.author.tag}: ${message.content}`);
+	} else {
+		console.log(`(dm) ${message.author.tag}: ${message.content}`);
+		const channel = client.channels.cache.get('1458866822961954909');
+
+		if (!anonimos.has(message.author.tag)) {
+			anonimos.set(message.author.tag, anonimos.size);
+		}
+
+		if (message.content.split('@everyone').length > 1 || message.content.split('@here').length > 1) {
+			await channel.send({content: `Anônimo ${anonimos.get(message.author.tag)} Tentou marcar geral (é o(a) ${message.author.tag})`});
+			return;
+		}
+
+		let messageText = message.content; 
+
+		if (message.content.includes('@')) {
+			const split = messageText.split('@');
+			messageText = '';
+
+			for (let i = 0; i < split.length; i++) {
+				messageText += split[i];
+			}
+		}
+
+		await channel.send({content: `\`Anônimo ${anonimos.get(message.author.tag)} :\` ${messageText}`});
+	}
+});
+
+client.login(token);
